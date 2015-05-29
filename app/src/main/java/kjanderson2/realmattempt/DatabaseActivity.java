b73e070b728e2005698d02c4c4b077808c10d943
KAITLYN ANDERSON <kaitlyn.anderson@uncorkedstudios.com>
2015-05-29 14:45:15 -0700

    Initial commit

---
+++ app/src/main/java/kjanderson2/realmattempt/DatabaseActivity.java
@@ -0,0 +1,86 @@
+package kjanderson2.realmattempt;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import io.realm.Realm;
+
+
+public class DatabaseActivity extends ActionBarActivity{
+
+    TextView idView;
+    EditText productBox;
+    EditText quantityBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database);
+
+        idView = (TextView) findViewById(R.id.productID);
+        productBox = (EditText) findViewById(R.id.productName);
+        quantityBox = (EditText) findViewById(R.id.productQuantity);
+    }
+
+    public void newProduct(View view){
+        Realm realm = Realm.getInstance(this);
+        realm.beginTransaction();
+
+        int quantity = Integer.parseInt(quantityBox.getText().toString());
+        Product product = new Product(productBox.getText().toString(), quantity);
+        Product realmProduct = realm.createObject(Product.class);
+
+        realmProduct.setId(product.getId());
+        realmProduct.setName(product.getName().toString());
+        realmProduct.setQuantity(quantity);
+
+        realm.commitTransaction();
+
+        productBox.setText("");
+        quantityBox.setText("");
+    }
+
+    public void lookupProduct (View view) {
+        Realm realm = Realm.getInstance(this);
+        realm.beginTransaction();
+
+        Product product = realm.where(Product.class)
+                               .contains("name", productBox.getText().toString()) //Not sure about this first parameter
+                               .findFirst();
+
+        if(product != null){
+            idView.setText(String.valueOf(product.getId()));
+            quantityBox.setText(String.valueOf(product.getQuantity()));
+        } else {
+            idView.setText("No Match Found");
+        }
+
+        realm.commitTransaction();
+    }
+
+    public void removeProduct(View view) {
+
+
+        Realm realm = Realm.getInstance(this);
+        realm.beginTransaction();
+
+        Product product = realm.where(Product.class)
+                .contains("name", productBox.getText().toString()) //Not sure about this first parameter
+                .findFirst();
+
+        boolean result = (product != null); //Unsure if a realm query will output null if none is found.
+        product.removeFromRealm();
+        if(result){
+            idView.setText("Record Deleted");
+            productBox.setText("");
+            quantityBox.setText("");
+        } else {
+            idView.setText("No Match Found");
+        }
+        realm.commitTransaction();
+    }
+
+}
---
+++ app/src/main/java/kjanderson2/realmattempt/Product.java
@@ -0,0 +1,51 @@
+package kjanderson2.realmattempt;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by kjanderson2 on 5/29/15.
+ */
+public class Product extends RealmObject{
+    private int id;
+    private String name;
+    private int quantity;
+
+    public Product(){
+
+    }
+
+    public Product(int id, String productname, int quantity) {
+        this.id = id;
+        this.name = productname;
+        this.quantity = quantity;
+    }
+
+    public Product(String productname, int quantity) {
+        this.name = productname;
+        this.quantity = quantity;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setName(String productname) {
+        this.name = productname;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+}
