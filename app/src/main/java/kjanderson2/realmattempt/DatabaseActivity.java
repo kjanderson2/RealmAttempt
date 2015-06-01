9e8a93f3534429d3b112b1f465857d906651f912
KAITLYN ANDERSON <kaitlyn.anderson@uncorkedstudios.com>
2015-06-01 14:58:01 -0700

    Small changes made like comments. No functionality changed.

--- app/src/main/java/kjanderson2/realmattempt/DatabaseActivity.java
+++ app/src/main/java/kjanderson2/realmattempt/DatabaseActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import io.realm.Realm;
 
 
@@ -17,7 +16,7 @@ public class DatabaseActivity extends ActionBarActivity{
     EditText quantityBox;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { // 5 lines
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
 
@@ -26,7 +25,7 @@ public class DatabaseActivity extends ActionBarActivity{
         quantityBox = (EditText) findViewById(R.id.productQuantity);
     }
 
-    public void newProduct(View view){
+    public void newProduct(View view){ // 12 lines
         Realm realm = Realm.getInstance(this);
         realm.beginTransaction();
 
@@ -45,27 +44,22 @@ public class DatabaseActivity extends ActionBarActivity{
         realm.commitTransaction();
     }
 
-    public void lookupProduct (View view) {
+    public void lookupProduct (View view) { //11 lines total
         Realm realm = Realm.getInstance(this);
         realm.beginTransaction();
-
         Product product = realm.where(Product.class)
-                               .contains("name", productBox.getText().toString()) //Not sure about this first parameter
+                               .contains("name", productBox.getText().toString())
                                .findFirst();
-
         if(product != null){
             idView.setText(String.valueOf(product.getId()));
             quantityBox.setText(String.valueOf(product.getQuantity()));
         } else {
             idView.setText("No Match Found");
         }
-
         realm.commitTransaction();
     }
 
-    public void removeProduct(View view) {
-
-
+    public void removeProduct(View view) { // 14 lines total
         Realm realm = Realm.getInstance(this);
         realm.beginTransaction();
 
